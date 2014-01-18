a11230970d4db87834a2142c016828f42fcac056
joaquin reyes <hola@jrltt.net>
2014-01-18 20:28:13 +0100

    Archivo que contiene todas las funciones

---
+++ funciones.php
@@ -0,0 +1,28 @@
+<?php 
+	/*
+	* Archivo que contiene todas las funciones del proyecto
+	* Autor: Joaquín Reyes Lettieri
+	* Version: 1.0
+	* Fecha: 18.01.14
+	*/
+ ?>
+<?php 
+	/*
+	* Muestra el menu principal
+	*/
+	function menu()
+	{
+?>
+	<div class="menu">
+		<h3>Menu</h3>
+		<ul>
+			<li><a href="index.php">Index</a></li>
+			<li><a href="register.php">Registrar persona</a></li>
+			<li><a href="regpro.php">Registrar proyecto</a></li>
+			<li><a href="list.php">Añadir personas a proyectos</a></li>
+			<li><a href="listpro.php">Gestionar proyectos</a></li>
+		</ul>
+	</div>
+<?php
+	}
+?>
