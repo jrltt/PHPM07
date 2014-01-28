43377f7ce29a6de6ead69c4bc1e61e1d58599325
Joaquin Reyes Lettieri <minimals86@gmail.com>
2014-01-28 21:55:55 +0100

    pdo base de datos

--- POO/BBDDpers.php
+++ POO/BBDDpers.php
@@ -17,18 +17,35 @@
 		/*
 		* Función para guardar la persona en la BBDD
 		*/
-		public function guardar()
-		{
-			//Conexion con la BBDD
-			$link = mysqli_connect("localhost", "root", "1234") or die ('Error'.mysqli_error($link));
-			mysqli_select_db($link, "test") or die ('Error, no se puede conectar');
-			//variables a guardar en la bbdd
-			$nomSave = $this->perSave->getNombre();
-			$fnSave = $this->perSave->getFNacimiento();
-			$genSave = $this->perSave->getGenero();
-			$sentencia = "INSERT INTO test.persona(nombre,fna,genero) VALUES ('$nomSave','$fnSave','$genSave')";
-			mysqli_query($link,$sentencia) or die ('Error en: '.$sentencia.' - '.mysqli_error($link) );
-			mysqli_close($link);
+		 public function guardar()
+		 {
+		// 	//Conexion con la BBDD
+		// 	$link = mysqli_connect("localhost", "root", "1234") or die ('Error'.mysqli_error($link));
+		// 	mysqli_select_db($link, "test") or die ('Error, no se puede conectar');
+		// 	//variables a guardar en la bbdd
+		// 	$nomSave = $this->perSave->getNombre();
+		// 	$fnSave = $this->perSave->getFNacimiento();
+		// 	$genSave = $this->perSave->getGenero();
+		// 	$sentencia = "INSERT INTO test.persona(nombre,fna,genero) VALUES ('$nomSave','$fnSave','$genSave')";
+		// 	mysqli_query($link,$sentencia) or die ('Error en: '.$sentencia.' - '.mysqli_error($link) );
+		// 	mysqli_close($link);
+		// }
+		$user = "root";
+		$pwd = "1234";
+			try
+			{
+				$nomSave = $this->perSave->getNombre();
+				$fnSave = $this->perSave->getFNacimiento();
+				$genSave = $this->perSave->getGenero();
+
+				$host = new PDO("mysql:host=localhost;dbname=test", $user, $pwd);
+				$host->exec("INSERT INTO test.persona(nombre,fn,genero) VALUES (`$this->perSave->getNombre()`,'$fnSave','$genSave')");
+				$host = null;
+			}
+			catch (PDOException $e)
+			{
+				echo $e->getMessage();
+			}
 		}
 	}
  ?>
--- POO/main.php
+++ POO/main.php
@@ -4,7 +4,7 @@
 	require_once('BBDDpers.php');
 	
 	//creo una persona
-	$per1 = new Persona("Simon","curioso","19860406");
+	$per1 = new Persona("Aroon","curioso","19860406");
 	//instancio la interfaz grafica
 	$visual = new GUIpersona($per1);
 	//la muestro
@@ -14,7 +14,7 @@
 	//utilizo la funcion para guardar
 	$toSave->guardar();
 	//creo otra
-	$per2 = new Persona('Maria','femenino','19900619');
+	$per2 = new Persona('Maooooo','femenino','19900619');
 	//asigno la nueva persona
 	$visual->setPersona($per2);
 	//la vuelvo a mostrar
